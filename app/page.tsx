e5bcf50497202d77a6d015a856bcd84734667105
Vishnuprakash P <78410291+vishkashpvp@users.noreply.github.com>
2025-10-13 22:39:42 +0530

    Minimal Landing Page (#2)
    * chore: add shadcn-ui default setup
    
    * feat(landing): setup hero and footer
    
    * feat: add dark mode support using next-themes
    
    * docs: add LICENSE and update README
    
    * docs: add CHANGELOG

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import AppFooter from "@/components/AppFooter";
+import AppHeader from "@/components/AppHeader";
+import LaunchHero from "@/components/LaunchHero";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen font-sans">
-      <h1>Hello World</h1>
+    <div className="flex flex-col min-h-dvh">
+      <AppHeader />
+
+      <main className="flex-1">
+        <section className="w-full max-w-3xl px-6 py-16 mx-auto md:py-24">
+          <LaunchHero />
+        </section>
+      </main>
+
+      <AppFooter />
     </div>
   );
 }
