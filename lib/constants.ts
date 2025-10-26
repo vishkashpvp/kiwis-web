3533957ea7e030858641fb8301448f4815ced5b9
Vishnuprakash P <78410291+vishkashpvp@users.noreply.github.com>
2025-10-26 22:48:44 +0530

    feat(profile): display authenticated user data (#13)

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,7 @@
 export const SCOPE_GMAIL_READONLY = "https://www.googleapis.com/auth/gmail.readonly";
+
+export const ICONS_URL = {
+  bitcoin: "https://upload.wikimedia.org/wikipedia/commons/4/46/Bitcoin.svg",
+  google: "https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg",
+  gmail: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg",
+};
